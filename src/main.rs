8ccdb7f7d25d2d15844745086345ba90b621adba
neilisaac <nisaac@waabi.ai>
2023-11-24 21:05:44 -0500

    toggle led

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,12 @@ use panic_probe as _;
 
 #[rtic::app(device = stm32f4xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
 mod app {
-    use stm32f4xx_hal::prelude::*;
-    use systick_monotonic::Systick;
+    use stm32f4xx_hal::{
+        self as hal,
+        gpio::{Output, PushPull},
+        prelude::*,
+    };
+    use systick_monotonic::{fugit::Duration, Systick};
 
     #[monotonic(binds = SysTick, default = true)]
     type MonotonicTimer = Systick<100>; // 100 Hz / 10 ms granularity
@@ -17,12 +21,13 @@ mod app {
 
     #[local]
     struct Local {
-        foo: i32,
+        led: hal::gpio::PG6<Output<PushPull>>,
     }
 
     #[init]
     fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
-        ctx.device
+        let _clocks = ctx
+            .device
             .RCC
             .constrain()
             .cfgr
@@ -33,11 +38,18 @@ mod app {
 
         let monotonics = init::Monotonics(Systick::new(ctx.core.SYST, 180_000_000));
 
-        startup::spawn().unwrap();
+        // let timer = ctx.device.TIM2.
+
+        blink::spawn().unwrap();
+
         defmt::info!("init!");
 
+        let gpiog = ctx.device.GPIOG.split();
+
         let shared = Shared {};
-        let local = Local { foo: 42 };
+        let local = Local {
+            led: gpiog.pg6.into_push_pull_output(),
+        };
 
         (shared, local, monotonics)
     }
@@ -50,8 +62,10 @@ mod app {
         }
     }
 
-    #[task(local = [foo], priority = 1)]
-    fn startup(ctx: startup::Context) {
-        defmt::info!("starting! {}", ctx.local.foo);
+    #[task(local = [led])]
+    fn blink(ctx: blink::Context) {
+        ctx.local.led.set_state(!ctx.local.led.get_state());
+
+        blink::spawn_after(Duration::<u64, 1, 100>::millis(1000)).unwrap();
     }
 }
